8ac198a02382300d5be63ad3e300da4e03904c13
Jack Hamer <jackhamer0990@gmail.com>
2023-12-05 23:59:19 +0200

    feat: add sepolia testnet

--- templates/nodejs/ethers5/src/utils/chains.ts
+++ templates/nodejs/ethers5/src/utils/chains.ts
@@ -15,9 +15,15 @@ export const ethereumMainnet: L1Chain = {
   rpcUrl: "https://rpc.ankr.com/eth",
   blockExplorerUrl: "https://etherscan.io",
 }
+export const ethereumSepolia: L1Chain = {
+  id: 11155111,
+  name: "Ethereum Sepolia Testnet",
+  rpcUrl: "https://rpc.ankr.com/eth_sepolia",
+  blockExplorerUrl: "https://sepolia.etherscan.io",
+}
 export const ethereumGoerli: L1Chain = {
   id: 5,
-  name: "Ethereum Goerli",
+  name: "Ethereum Goerli Testnet",
   rpcUrl: "https://rpc.ankr.com/eth_goerli",
   blockExplorerUrl: "https://goerli.etherscan.io",
 }
@@ -29,18 +35,25 @@ export const ethereumDockerizedNode: L1Chain = {
 
 export const zkSyncMainnet: Chain = {
   id: 324,
-  name: "zkSync Era",
+  name: "zkSync",
   rpcUrl: "https://mainnet.era.zksync.io",
   blockExplorerUrl: "https://explorer.zksync.io",
   blockExplorerApi: "https://block-explorer-api.mainnet.zksync.io", // Docs: https://block-explorer-api.mainnet.zksync.io/docs
   l1: ethereumMainnet,
 }
-export const zkSyncTestnet: Chain = {
+export const zkSyncSepoliaTestnet: Chain = {
+  id: 300,
+  name: "zkSync Sepolia Testnet",
+  rpcUrl: "https://sepolia.era.zksync.dev",
+  blockExplorerUrl: "https://sepolia.explorer.zksync.io",
+  blockExplorerApi: "https://block-explorer-api.sepolia.zksync.dev", // Docs: https://block-explorer-api.sepolia.zksync.dev/docs
+}
+export const zkSyncGoerliTestnet: Chain = { // deprecated network
   id: 280,
-  name: "zkSync Era Testnet",
+  name: "zkSync Goerli Testnet",
   rpcUrl: "https://testnet.era.zksync.dev",
   blockExplorerUrl: "https://goerli.explorer.zksync.io",
-  blockExplorerApi: "https://block-explorer-api.testnets.zksync.dev", // Docs: https://block-explorer-api.testnets.zksync.dev
+  blockExplorerApi: "https://block-explorer-api.testnets.zksync.dev", // Docs: https://block-explorer-api.testnets.zksync.dev/docs
 }
 
 export const zkSyncDockerizedNode: Chain = {
@@ -62,9 +75,10 @@ export const zkSyncInMemoryNode: Chain = {
 
 export const chains: Chain[] = [
   zkSyncMainnet,
-  zkSyncTestnet,
+  zkSyncSepoliaTestnet,
+  zkSyncGoerliTestnet,
   zkSyncDockerizedNode,
   zkSyncInMemoryNode
 ]
 
-export const defaultChain = zkSyncTestnet;
+export const defaultChain = zkSyncSepoliaTestnet;
